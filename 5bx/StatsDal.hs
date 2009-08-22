6390ccc0dfc4f9bfe6f933348f0228c57e36eecc
Austin King <shout@ozten.com>
2009-08-22 04:59:24 -0700

    Learning the hard way, you can't escape the IO Monad

--- 5bx/StatsDal.hs
+++ 5bx/StatsDal.hs
@@ -1,6 +1,8 @@
 module StatsDal where
 
-import Database.HDBC (fromSql, SqlValue)
+import Database.HDBC (fromSql, SqlValue, quickQuery')
+import Database.HDBC.MySQL (MySQLConnectInfo)
+import Database.HDBC.Types (disconnect, IConnection)
 
 import Stats
 
@@ -15,3 +17,13 @@ fromResults [i, w, st, si, b, p, c] = Stats id weight stretches situps backexts 
             backexts = f b
             pressups = f p
             chart = f c
+
+currentStat :: (IConnection a) => a -> IO Stats
+currentStat conn = query conn >>=
+                   process
+
+process :: [[SqlValue]] -> IO Stats
+process rs = return $ fromResults $ head rs
+
+query :: (IConnection a) => a -> IO [[SqlValue]]
+query conn = quickQuery' conn "SELECT id, weight, stretches, situps, backexts, pressups, chart FROM exercise_records ORDER BY id DESC LIMIT 1" []
