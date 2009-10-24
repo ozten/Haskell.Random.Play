369a0683d28efe5bf55b4c0b3777d499da40399f
Austin King <shout@ozten.com>
2009-10-24 05:34:19 -0700

    Removed console calls

--- CYOA/behavior.js
+++ CYOA/behavior.js
@@ -1,8 +1,6 @@
 var submitForm = false;
-$(document).ready(function(){
-    
-    $('input[name=page_name]').keyup(function(evt){
-            console.info("keyup");
+$(document).ready(function(){    
+    $('input[name=page_name]').keyup(function(evt){            
             var v = $('input[name=page_name]').attr("value");                
             v = v.replace(/[^.a-zA-Z0-9_-]/, '');            
             $('input[name=page_name]').attr("value", v);
@@ -10,21 +8,17 @@ $(document).ready(function(){
         })
         .focus();
     $('input[name=page_name]').keypress(function(evt){
-        console.info("keypress Seeing ", evt.keyCode);
-            if (evt.keyCode == 13) {
-                submitForm = false;
-                return false;
-            }
+        if (evt.keyCode == 13) {
+            submitForm = false;
+            return false;
+        }
+        return true;
     });
-    $('#Save-btn, #Cancel-btn').click(function(){
-        console.log("handeling click SubmitFor is ", submitForm);
+    $('#Save-btn, #Cancel-btn').click(function(){        
         submitForm = true;
     });
     
-    $('#create-page-form').submit(function(evt, f){
-        window.evt = evt;
-        window.f = f;
-        console.log("handeling submit SubmitForm is ", submitForm);
+    $('#create-page-form').submit(function(evt, f){        
         return submitForm;
     });
 });
